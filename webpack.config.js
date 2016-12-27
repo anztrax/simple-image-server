f96e52e3fb097e85af0e6c99193902ababbfa210
andrew ananta <kuro.ketanitem@gmail.com>
2016-12-27 22:30:13 +0700

    1. add build server and client using webpack , add css-module-require-hook when render css 2. add simple react router

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var fs = require('fs');
+var combineLoaders = require('webpack-combine-loaders');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const config = require('./package').config;
+
+module.exports = {
+  entry: './src/client.js',
+  output: {
+    path : 'public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.css$/i,
+        loader: ExtractTextPlugin.extract('style',
+          `css?modules&localIdentName=${config.css}!postcss`),
+      },
+    ]
+  },
+  postcss: [
+    // small sugar for CSS
+    require('postcss-font-magician'),
+    require('autoprefixer'),
+  ],
+  plugins: [
+    new ExtractTextPlugin('common.css', {
+      allChunks: true
+    }),
+    new NpmInstallPlugin({
+      cacheMin: 999999,
+      saveDev: true,
+      saveExact: true,
+    }),
+  ]
+};
