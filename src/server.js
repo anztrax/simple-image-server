9d0953eede583b4fce9a5c34df8dc25cc3b2b501
andrew ananta <kuro.ketanitem@gmail.com>
2016-12-28 00:14:47 +0700

    initial commit for server

--- src/server.js
+++ src/server.js
@@ -1,28 +1,19 @@
 import express from 'express';
-import path from 'path';
+import bodyParser from 'body-parser';
 import compression from 'compression';
+
 const app = express();
 app.use(compression());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
-
-// send all requests to index.html so browserHistory works
-app.get('*', (req, res) => {
-  match({ routes, location: req.url }, (err, redirect, props) => {
-    if (err) {
-      res.status(500).send(err.message)
-    } else if (redirect) {
-      res.redirect(redirect.pathname + redirect.search)
-    } else if (props) {
-      // hey we made it!
-      const appHtml = renderToString(<RouterContext {...props}/>);
-      res.send(renderPage(appHtml))
-    } else {
-      res.status(404).send('Not Found')
-    }
-  })
+const PORT = process.env.PORT || 3020;
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
 });
+app.use('/api', router);
 
-const PORT = process.env.PORT || 3010;
-app.listen(PORT, function() {
-  console.log('Production Express server running at localhost:' + PORT)
-})
+app.listen(PORT, ()=> {
+  console.log('Magic start on port ' + PORT);
+});
